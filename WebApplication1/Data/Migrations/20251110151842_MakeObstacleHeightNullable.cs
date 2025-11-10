1325f6da1c882d27465c7535d7e2831f25030453
Prebenzenator <Prebenj@uia.no>
2025-11-10 19:17:57 +0100

    Helt fornyet

---
+++ WebApplication1/Data/Migrations/20251110151842_MakeObstacleHeightNullable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeObstacleHeightNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ObstacleHeight",
+                table: "Obstacles",
+                type: "decimal(65,30)",
+                nullable: true,  // Changed to nullable
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldNullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ObstacleHeight",
+                table: "Obstacles",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)",
+                oldNullable: true);
+        }
+    }
+}
