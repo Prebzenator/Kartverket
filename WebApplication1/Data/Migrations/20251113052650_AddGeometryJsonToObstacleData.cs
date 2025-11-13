edd91405c8151b99ce8e15b81ee931f28e0a8e82
MFM4 <madsfmalmin@gmail.com>
2025-11-13 14:54:34 +0100

    Added map features

---
+++ WebApplication1/Data/Migrations/20251113052650_AddGeometryJsonToObstacleData.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeometryJsonToObstacleData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GeometryJson",
+                table: "Obstacles",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GeometryJson",
+                table: "Obstacles");
+        }
+    }
+}
